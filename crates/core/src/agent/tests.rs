01e43744042ce075113f2c949f7328d66782760f
Cyandev <unixzii@gmail.com>
2026-02-12 12:21:23 +0800

    Add test for agent retries

--- crates/core/src/agent/tests.rs
+++ crates/core/src/agent/tests.rs
@@ -1,4 +1,5 @@
 use std::future::ready;
+use std::sync::atomic::{self, AtomicBool};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
@@ -163,4 +164,47 @@ async fn test_tool_call() {
     assert_eq!(tool_call_requests.len(), 2);
     assert_eq!(tool_call_requests[0], "Lists all todos");
     assert_eq!(tool_call_requests[1], "Lists all calendar events");
+}
+
+#[tokio::test(start_paused = true)]
+async fn test_retry() {
+    let mut model_provider = TestModelProvider::default();
+    model_provider.add_user_input_step();
+    model_provider.add_assistant_response_step(
+        PresetResponse::with_events([PresetEvent::MessageDelta(
+            "Hi".to_owned(),
+        )])
+        .with_failures(3),
+    );
+
+    let on_error_triggered = Arc::new(AtomicBool::new(false));
+    let transcripts = Arc::new(Mutex::new(String::new()));
+    let (idle_tx, mut idle_rx) = watch::channel::<bool>(false);
+
+    let agent = AgentBuilder::with_model_provider(model_provider)
+        .on_transcript({
+            let transcripts = Arc::clone(&transcripts);
+            move |transcript, source| {
+                if source.is_assistant() {
+                    transcripts.lock().unwrap().push_str(transcript);
+                }
+            }
+        })
+        .on_error({
+            let on_error_triggered = Arc::clone(&on_error_triggered);
+            move |_| {
+                on_error_triggered.store(true, atomic::Ordering::Relaxed);
+            }
+        })
+        .on_idle(move || {
+            idle_tx.send(true).unwrap();
+        })
+        .build();
+    agent.enqueue_user_input("Hello");
+
+    idle_rx.wait_for(|v| *v).await.unwrap();
+
+    let transcripts = transcripts.lock().unwrap();
+    assert_eq!(*transcripts, "Hi");
+    assert_eq!(on_error_triggered.load(atomic::Ordering::Relaxed), true);
 }
